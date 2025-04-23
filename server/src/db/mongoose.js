f70ec248fdeb1d2f6f6b5f2d4161866adcf28280
Nidhin Ninan <46945393+nidhinninan@users.noreply.github.com>
2025-04-23 11:12:25 -0500

    Update mongoose.js

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const uri = process.env.MONGO_URI;
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(uri, {
   useNewUrlParser: true,
-  useCreateIndex: true,
-});
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
